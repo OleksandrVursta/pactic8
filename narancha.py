794829bdac5fed7da5bc429ae30af1a766a79f72
Alexander <oleksandr.i.vyrsta@ukg.edu.ua>
2024-05-02 13:05:31 +0300

    Додано функцію обчислення ділення

---
+++ narancha.py
@@ -0,0 +1,9 @@
+Fugo = int(input("Перше число"))
+if Fugo <= 0:
+    print("Число менше за 0")
+else:
+    Purple_haze = int(input("Друге число"))
+    if Purple_haze <= 0:
+        print("Друге число менше за 0")
+    else:
+        print(Fugo/Purple_haze)
